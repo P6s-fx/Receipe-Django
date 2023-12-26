cfe99e017378dcdde4cb6cf512e66a17de3af521
P6s-fx <param.corpid@gmail.com>
2023-12-26 22:56:12 +0530

    search fuctionality of data done.

--- DJango/Dweb/veggie/views.py
+++ DJango/Dweb/veggie/views.py
@@ -20,6 +20,11 @@ def recipes(request):
         return redirect('/recipes/')
     
     queryset = Recipes.objects.all()
+
+    if request.GET.get('search'):
+        queryset = queryset.filter(recipe_name__icontains = request.GET.get('search'))
+        queryset = queryset.filter(recipe_description__icontains = request.GET.get('search'))
+
     context = {'recipes' : queryset}
     return render(request, 'recipes.html', context)
 
@@ -27,3 +32,24 @@ def delete_recipe(request,id):
     queryset = Recipes.objects.get(id = id)
     queryset.delete()
     return redirect('/recipes/')
+
+def update_recipe(request,id):
+    queryset = Recipes.objects.get(id = id)
+
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        recipe_image = request.FILES.get('recipe_image')
+
+        queryset.recipe_name = recipe_name
+        queryset.recipe_description = recipe_description
+        
+        if recipe_image :
+            queryset.recipe_image = recipe_image
+            
+        queryset.save()
+        return redirect('/recipes/')
+    context = {'recipe' : queryset}
+    return render(request, 'update_recipe.html', context)
+
