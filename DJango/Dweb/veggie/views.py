2bbad617bf79ca3d7873db86144b68c983c885bb
P6s-fx <param.corpid@gmail.com>
2024-01-02 22:07:47 +0530

    Completing authentication

--- DJango/Dweb/veggie/views.py
+++ DJango/Dweb/veggie/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/register/')
 def recipes(request):
-    
     if request.method == "POST":
         data = request.POST
         recipe_name = data.get('recipe_name')
@@ -58,7 +60,31 @@ def update_recipe(request,id):
 
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    
+        if not User.objects.filter(username = username).exists():
+            messages.error(request , 'Invalid Username')
+            return redirect('/login/')
+
+        user = authenticate(username = username,password = password)
+
+        if user is None:
+            messages.error(request,'Invalid Password!!!')
+            return redirect('/login/')
+        else:
+            login(request,user)
+            return redirect('/recipes/')
+        
+
+
     return render(request, 'Login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login/')
+
 
 def register_page(request):
     if request.method == "POST":
