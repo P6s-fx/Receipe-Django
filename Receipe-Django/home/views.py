9deb4f432887212c11372abea6a0c6b1904ecbaf
Param <111742883+P6s-fx@users.noreply.github.com>
2024-05-01 18:19:19 +0530

    changing local location of the project

---
+++ Receipe-Django/home/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    people = [
+        {'name' : 'param','age': 21},
+        {'name' : 'Sameer','age': 25},
+        {'name' : 'Krishna','age': 22},
+        {'name' : 'Ram','age': 26},
+        {'name' : 'Sita','age': 41},
+    ]
+        
+    return render(request , "home/index.html",context={'people' : people})
+
+def about(request):
+    contaxt = {'page' : 'About'}
+
+    return render(request , "home/about.html")
+
+def contact(request):
+    contaxt = {'page' : 'Contact'}
+    return render(request , "home/contact.html")
